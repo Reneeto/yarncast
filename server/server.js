195b5c1df9f0c8e9798494e0424c50956390d015
Renee Toscan <renee.toscan@outlook.com>
2023-06-24 14:37:44 -0700

    server is working

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 
-app.get('/api', (req, res) => {
-  res.json({message: 'hello from server!'})
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../src/index.html'))
 })
 
 app.listen(PORT, () => {
