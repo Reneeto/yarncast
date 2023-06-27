de648bdc5a8c96bff08f852b7e5ce19e535c8705
Renee Toscan <renee.toscan@outlook.com>
2023-06-26 19:10:12 -0700

    Creating basic react app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Display from "./components/Display";
+import Filters from "./components/Filters";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Display />
       <Footer />
     </div>
   );
---
+++ src/components/Filters.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+
+export default function Filters() {
+  return (
+    <div className="filters">
+      <DateRangeCalendar />
+    </div>
+  );
+}
