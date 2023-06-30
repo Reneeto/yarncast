728fbb721f016d74560d40dd448cebbf868a1558
Renee Toscan <renee.toscan@outlook.com>
2023-06-29 18:40:15 -0700

    getting rid of stuff i don't need

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import TableDatePicker from "./DatePicker";
-import ColorPicker from "./ColorPicker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
